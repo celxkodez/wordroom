0e3ee265fb97ddce7e44c3b8239796518aa679d2
celxkodez <decele2011@gmail.com>
2020-01-28 00:11:37 +0100

    details updated

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
 
   "database" => [
 
-      "connection" => 'mysql:us-cdbr-iron-east-05.cleardb.net',
+      "connection" => 'mysql:host=us-cdbr-iron-east-05.cleardb.net',
       "username" => 'bc64030e691a2b',
       "password" => '875647f1',
       "dbname"  => 'heroku_0915f6743244488',
