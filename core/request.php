73d7d20f0478df4417037a6ac0a69b1bafdc8b1f
celxkodez <decele2011@gmail.com>
2020-01-28 00:39:46 +0100

    depencies

--- core/request.php
+++ core/request.php
@@ -8,8 +8,8 @@ class Request {
 
     // die(var_dump(trim($_SERVER['REQUEST_URI'], '/' )));
 
-    return trim(
-      parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH),  '/' );
+    return die(var_dump(trim(
+      parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH),  '/' )));
   
   }
   public static function method(){
