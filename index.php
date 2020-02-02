6ee031c4c9490cd0b16b0c3b575df4ac219ca63c
celxkodez <decele2011@gmail.com>
2020-02-02 01:25:51 +0100

    trial

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $database = require 'core/bootstrap.php';
 $_SERVER["SCRIPT_NAME"] = "/index.php" ;
 $_SERVER["HOME"] = "app/index.php" ;
 $_SERVER["HTTP_CONNECTION"] = "keep-alive";
+$_SERVER["HEROKU_APP_DIR"] = "/app/index.php" ;
 
 
 //echo trim($_SERVER['REQUEST_URI'], '/' );
