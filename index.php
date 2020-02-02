6c9347f889bf6497fed737d3788625506aca4b45
celxkodez <decele2011@gmail.com>
2020-02-02 01:21:47 +0100

    trial

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $database = require 'core/bootstrap.php';
 
 $_SERVER["SCRIPT_NAME"] = "/index.php" ;
 $_SERVER["HOME"] = "app/index.php" ;
+$_SERVER["HTTP_CONNECTION"] = "keep-alive";
 
 
 //echo trim($_SERVER['REQUEST_URI'], '/' );
