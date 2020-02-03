c3edf7338d0e4b9028267bc0e216ba92a10cc11d
celxkodez <decele2011@gmail.com>
2020-02-03 11:47:36 +0100

    some more debugging

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 
 $database = require 'core/bootstrap.php';
 
-$_SERVER["SCRIPT_NAME"] = "/index.php" ;
-$_SERVER["HOME"] = "app/index.php" ;
-$_SERVER["HTTP_CONNECTION"] = "keep-alive";
-$_SERVER["HEROKU_APP_DIR"] = "/app/index.php" ;
-$_SERVER["PWD"] = "/app/index.php";
-$_SERVER["DOCUMENT_ROOT"] = "/app/index.php";
+// $_SERVER["SCRIPT_NAME"] = "/index.php" ;
+// $_SERVER["HOME"] = "app/index.php" ;
+// $_SERVER["HTTP_CONNECTION"] = "keep-alive";
+// $_SERVER["HEROKU_APP_DIR"] = "/app/index.php" ;
+// $_SERVER["PWD"] = "/app/index.php";
+// $_SERVER["DOCUMENT_ROOT"] = "/app/index.php";
 
 
 //echo trim($_SERVER['REQUEST_URI'], '/' );
